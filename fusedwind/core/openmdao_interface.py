db76a81409cfd55ec5250c47baa2aadd5b6596db
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-06-25 13:56:54 +0200

    * refactoring the fused decorator interface.  * fixing the weird bug that was caused by a not so OrderedDict  * moving things around

--- fusedwind/core/openmdao_interface.py
+++ fusedwind/core/openmdao_interface.py
@@ -7,66 +7,97 @@ from openmdao.core.component import Component
 Inputs = OrderedDict
 Outputs = OrderedDict
 
+
+class FUSEDComponent(Component):
+    def __init__(self, inputs, outputs, wrapped_function):
+        super(FUSEDComponent, self).__init__()
+        self.inputs = inputs
+        self.outputs = outputs
+        self.func = wrapped_function
+        self.ref_inputs = []
+        self.ref_outputs = []
+
+        # Create the inputs
+        for i in self.inputs:
+            if isinstance(i, tuple):
+                name, v = i
+            else:
+                v = i
+                name = v['name']
+
+            self.ref_inputs.append(name)
+            self.add_param(name, v['val'])
+
+        # Create the outputs
+        for o in self.outputs:
+            if isinstance(o, tuple):
+                name, v = o
+            else:
+                v = o
+                name = v['name']
+            self.ref_outputs.append(name)
+            self.add_output(name, v['val'])
+
+
+    def params2inputs(self, params):
+        """Convert the openmdao  `params` pseudo dict into a fusedwind compatible dict"""
+        return [params[k] for k in self.ref_inputs]
+
+    def outputs2unknowns(self, fused_outputs):
+        """Convert the a fusedwind compatible output list into an `unknowns` pseudo dict"""
+        if len(self.ref_outputs) == 1:
+            # If there is only one output, then we wrap it inside a function
+            return {self.ref_outputs[0]: fused_outputs}
+        else:
+            return dict(zip(self.ref_outputs, fused_outputs))
+
+    def solve_nonlinear(self, params, unknowns, resids):
+        new_unknowns = self.outputs2unknowns(self.func(*self.params2inputs(params)))
+        for k,v in new_unknowns.items():
+            unknowns[k] = v
+
+
+    def __call__(self, *args, **kwargs):
+        """Call the component in a pythonic way:
+        [output_1, ..., output_n] = func(input_1, ..., input_n)
+        If only part of the inputs are specified, the component will use the default values or the previously
+        defined values.
+
+        :param args:    list
+                        inputs defined by the wrapper. The order corresponds to the order indicated in the
+                        wrapper decorator
+        :param kwargs:  dict
+                        inputs defined by the wrapper.
+        :return:
+        """
+        params = self._params_dict
+        unknowns = self._unknowns_dict
+        resids = OrderedDict()
+        input_keys = list(params.keys())
+
+        # TODO: verify that the args are not replaced by the kwargs
+        if len(args)>0 and len(kwargs)>0:
+            raise NotImplementedError('Only use args or kwargs independently, not both at the same time.')
+
+        for i, a in enumerate(args):
+            k = input_keys[i]
+            params[k] = a
+        for k,v in kwargs.items():
+            params[k] = v
+        self.solve_nonlinear(params, unknowns, resids)
+
+        values = list(unknowns.values())
+        if len(values) > 1:
+            return [unknowns[k] for k in self.ref_outputs]
+        else:
+            return values[0]
+
+
 def fused_func(inputs=Inputs(), outputs=Outputs()):
     def _outer_wrapper(wrapped_function):
-        ## Create the component that wrapp the function
-        class _C(Component):
-            def __init__(self):
-                super().__init__()
-                for k, v in inputs.items():
-                    self.add_param(k, v['val'])
-
-                for k, v in outputs.items():
-                    self.add_output(k, v['val'])
-
-            def solve_nonlinear(self, params, unknowns, resids):
-                results = wrapped_function(*[params[k] for k in inputs.keys()])
-                if not isinstance(results, list):
-                    unknowns[list(outputs.keys())[0]] = results
-                else:
-                    for k, v in zip(outputs.keys(), results):
-                        unknowns[k] = v
-
-            def __call__(self, *args, **kwargs):
-                """Call the component in a pythonic way:
-                [output_1, ..., output_n] = func(input_1, ..., input_n)
-                If only part of the inputs are specified, the component will use the default values or the previously
-                defined values.
-
-                :param args:    list
-                                inputs defined by the wrapper. The order corresponds to the order indicated in the
-                                wrapper decorator
-                :param kwargs:  dict
-                                inputs defined by the wrapper.
-                :return:
-                """
-                params = self._params_dict
-                unknowns = self._unknowns_dict
-                resids = OrderedDict()
-                input_keys = list(params.keys())
-                # TODO: verify that the args are not replaced by the kwargs
-                if len(args)>0 and len(kwargs)>0:
-                    raise NotImplementedError('Only use args or kwargs independently, not both at the same time.')
-
-                for i, a in enumerate(args):
-                    k = input_keys[i]
-                    params[k] = a
-                for k,v in kwargs.items():
-                    params[k] = v
-                self.solve_nonlinear(params, unknowns, resids)
-
-                values = list(unknowns.values())
-                if len(values) > 1:
-                    return [v for v in unknowns.values()]
-                else:
-                    return values[0]
-
-
-        _C.__name__ = 'F'+ wrapped_function.__name__
-
         ## Now returns a function that initialize the component
         def _initialize(*args, **kwargs):
-            c = _C()
+            c = FUSEDComponent(inputs, outputs, wrapped_function)
             input_keys = list(c._params_dict.keys())
             for i, a in enumerate(args):
                 k = input_keys[i]
--- fusedwind/core/test/test_openmdao_interface.py
+++ fusedwind/core/test/test_openmdao_interface.py
@@ -17,43 +17,34 @@ class TestFused_func(unittest.TestCase):
     def test_one_output(self):
 
         @fused_func(
-            Inputs(
-                ws = wind_speed(4.0),
-                wd = wind_direction(270.0)),
-            Outputs(
-                p = power))
-        def my_silly_func(u, d):
-            return u**3.0 * sin(d * pi/180.)
-
+            inputs = [wind_speed, wind_direction],
+            outputs = [power])
+        def my_silly_func(ws, wd):
+            return ws**3.0 * sin(wd * pi/180.0)
         a = my_silly_func(3.0, 0.0)
-        self.assertAlmostEqual(a(5.0, 90.0), 5.0**3.0)
+        self.assertAlmostEqual(a(5.0, 90.0), 5.0**3.0 * sin(90.0 * pi/180.0))
 
     def test_several_outputs(self):
 
         @fused_func(
-            Inputs(
-                ws = wind_speed(4.0),
-                wd = wind_direction(270.0)),
-            Outputs(
-                p1 = power,
-                p2 = power,
-                p3 = power))
-        def my_silly_func(u, d):
-            return [i * u**3.0 * sin(d * pi/180.) for i in range(3)]
+            inputs = [wind_speed, wind_direction],
+            outputs = [('p1',power),('p2', power),('p3', power)])
+        def my_silly_func(ws, wd):
+            return [i * ws**3.0 * sin(wd * pi/180.) for i in range(3)]
 
         a = my_silly_func(3.0, 0.0)
-        for i in range(3):
-            self.assertAlmostEqual(a(5.0, 90.0)[i], i*5.0**3.0)
+
+        for k in ['p1', 'p2', 'p3']:
+            self.assertTrue(k in a._unknowns_dict)
+
+        self.assertAlmostEqual(a(5.0, 90.0), [i*5.0**3.0 for i in range(3)])
 
     def test_call_error(self):
         @fused_func(
-            Inputs(
-                ws = wind_speed(4.0),
-                wd = wind_direction(270.0)),
-            Outputs(
-                p = power))
-        def my_silly_func(u, d):
-            return u**3.0 * sin(d * pi/180.)
+            inputs = [wind_speed, wind_direction],
+            outputs = [power])
+        def my_silly_func(ws, wd):
+            return ws**3.0 * sin(wd * pi/180.)
 
         a = my_silly_func(3.0, 0.0)
         with self.assertRaises(NotImplementedError,
@@ -66,13 +57,10 @@ class TestOpenMDAOInterface(unittest.TestCase):
 
     def setUp(self):
         @fused_func(
-            Inputs(
-                ws = wind_speed(4.0),
-                wd = wind_direction(270.0)),
-            Outputs(
-                p = power))
-        def my_silly_func(u, d):
-            return u**3.0 * sin(d * pi/180.0)
+            inputs = [wind_speed, wind_direction],
+            outputs = [power])
+        def my_silly_func(ws, wd):
+            return ws**3.0 * sin(wd * pi/180.0)
 
         self.func = my_silly_func()
 
@@ -81,29 +69,53 @@ class TestOpenMDAOInterface(unittest.TestCase):
         pb.root.add('a', self.func)
         pb.setup()
         self.assertTrue(hasattr(pb.root,'a'))
-        self.assertTrue('ws' in pb.root.a.params)
+        self.assertTrue('power' in pb.root.a.unknowns)
 
     def test_promotes(self):
         pb = Problem(root=Group())
         pb.root.add('a', self.func, promotes=['*'])
         pb.setup()
-        self.assertTrue('p' in pb.root.unknowns)
+        self.assertTrue('power' in pb.root.unknowns)
 
     def test_connection(self):
         pb = Problem(root=Group())
         pb.root.add('a', self.func, promotes=['*'])
-        pb.root.add('eq', ExecComp('y = p*2.0'), promotes=['*'])
+        pb.root.add('eq', ExecComp('y = power*2.0'), promotes=['*'])
         pb.setup()
-        self.assertEqual(pb.root.connections, {'eq.p':'a.p'})
+        self.assertEqual(pb.root.connections, {'eq.power':'a.power'})
 
     def test_run(self):
         pb = Problem(root=Group())
-        pb.root.add('iws', ParamComp('ws', 5.0), promotes=['*'])
-        pb.root.add('iwd', ParamComp('wd', 90.0), promotes=['*'])
+        pb.root.add('iws', ParamComp('wind_speed', 5.0), promotes=['*'])
+        pb.root.add('iwd', ParamComp('wind_direction', 90.0), promotes=['*'])
         pb.root.add('a', self.func, promotes=['*'])
-        pb.root.add('eq', ExecComp('y = p*2.0'), promotes=['*'])
+        pb.root.add('eq', ExecComp('y = power*2.0'), promotes=['*'])
         pb.setup()
         pb.run()
         result = 5.0**3.0 * sin(90.0 * pi / 180.0)  * 2.0
+        self.assertAlmostEqual(pb.root.unknowns['y'], result)
+
+class TestSpecialOpenMDAO(unittest.TestCase):
+    def test_same_types(self):
+        @fused_func(
+            inputs = [('ws1', wind_speed),
+                      ('ws2', wind_speed),
+                      wind_direction],
+            outputs = [power])
+        def my_silly_func(ws1, ws2, wd):
+            return ((ws1 + ws2)/2.0)**3.0 * sin(wd * pi/180.0)
+
+        pb = Problem(root=Group())
+        ws1 = 4.0
+        ws2 = 25.0
+        wd = 45.0
+        pb.root.add('iws1', ParamComp('ws1', ws1), promotes=['*'])
+        pb.root.add('iws2', ParamComp('ws2', ws2), promotes=['*'])
+        pb.root.add('iwd', ParamComp('wind_direction', wd), promotes=['*'])
+        pb.root.add('a', my_silly_func(), promotes=['*'])
+        pb.root.add('eq', ExecComp('y = power*2.0'), promotes=['*'])
+        pb.setup()
+        pb.run()
+        result = (((ws1 + ws2)/2.0)**3.0 * sin(wd * pi/180.0)) * 2.0
         self.assertAlmostEqual(pb.root.unknowns['y'], result)
 
