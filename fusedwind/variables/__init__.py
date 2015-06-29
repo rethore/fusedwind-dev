c586c6e0728881cb26637285a886ebb16314cfd1
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-06-29 16:00:43 +0200

    Adding a dictionary containing all the fusedwind default variables

--- fusedwind/variables/__init__.py
+++ fusedwind/variables/__init__.py
@@ -4,10 +4,16 @@ import yaml
 from path import Path
 from fusedwind.core.fused_variable import FUSEDVar
 
+fall = {}
 # Loop on all the yaml files and add the base keys as local dictionaries
 for f in Path(os.path.dirname(os.path.realpath(__file__))).files('*yaml'):
     dic = yaml.load(f.text())
+    fil = os.path.basename(f.split('.')[0])
+    fall[fil] = {}
     for k, v in dic.items():
+        v['name'] = k
         exec('%s=FUSEDVar(**%s)'%(k, repr(v)))
+        fall[fil][k] = FUSEDVar(**v)
+
     #globals().update(dic)
 
