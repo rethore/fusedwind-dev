56cbf62adf2adcecc962f6c8ea7d5d5f939572c9
fzahle <frza@dtu.dk>
2015-11-02 15:21:02 +0100

    deleted files that got merged from experimental core_interface branch

--- fusedwind/variables/test/test_load.py
+++
@@ -1,9 +0,0 @@
-
-__author__ = 'pire'
-import unittest
-
-class TestLoadInit(unittest.TestCase):
-    def test_import(self):
-        from fusedwind.variables import wind_speed
-        print(wind_speed)
-        print(wind_speed.desc)
