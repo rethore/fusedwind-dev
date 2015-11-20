54f07a32c5789aff5c175883644c998a4b292e98
fzahle <frza@dtu.dk>
2015-11-20 13:12:04 +0100

    a few more tests of FUSEDProblem

--- fusedwind/core/test/test_problem_builder.py
+++ fusedwind/core/test/test_problem_builder.py
@@ -44,13 +44,93 @@ class TestProblemBuilder(unittest.TestCase):
 
         os.remove('simple.yml')
 
-    def test_pb(self):
+    def test_simple(self):
 
-        p = configure()
+        c = {'root':
+                {'class': 'Group',
+                 'components':
+                    [
+                    {'name': 'simple',
+                     'class': 'fusedwind.core.test.simple_comps.SimpleComp',
+                     'parameters':
+                            {'multiplier': 4.},
+                      'promotes': ['*']}]}}
+
+        yaml.dump(c, open('simple.yml', 'w'))
+
+        p = FUSEDProblem(filename='simple.yml')
         p.setup()
         p.run()
         self.assertEqual(p['x'], 3.)
         self.assertEqual(p['y'], 12.)
+
+    def test_indep(self):
+
+        c = {'root':
+                {'class': 'Group',
+                 'components':
+                    [
+                    {'name': 'x_c',
+                    'class': 'IndepVarComp',
+                    'parameter': ['p0', 6.],
+                    'promotes': ['*']}]}}
+
+        yaml.dump(c, open('simple.yml', 'w'))
+
+        p = FUSEDProblem(filename='simple.yml')
+        p.setup()
+        p.run()
+        self.assertEqual(p['p0'], 6.)
+
+    def test_exec(self):
+
+        c = {'root':
+                {'class': 'Group',
+                 'components':
+                    [
+                    {'name': 'x_div',
+                    'class': 'ExecComp',
+                    'expr': ['p1 = p0 * 4', 'x = p1 / 2.'],
+                    'parameters': {'p0': 25.},
+                    'promotes': ['*']}]}}
+
+        yaml.dump(c, open('simple.yml', 'w'))
+
+        p = FUSEDProblem(filename='simple.yml')
+        p.setup()
+        p.run()
+        self.assertEqual(p['p0'], 25.)
+        self.assertEqual(p['x'], 50.)
+
+    def test_combined(self):
+
+        c = {'root':
+                {'class': 'Group',
+                 'components':
+                    [
+                    {'name': 'x_c',
+                    'class': 'IndepVarComp',
+                    'parameter': ['p0', 6.],
+                    'promotes': ['*']},
+                    {'name': 'x_div',
+                    'class': 'ExecComp',
+                    'expr': ['x = p0 / 2.'],
+                    'parameters': {'p0': 25.},
+                    'promotes': ['*']},
+                    {'name': 'simple',
+                     'class': 'fusedwind.core.test.simple_comps.SimpleComp',
+                     'parameters':
+                            {'multiplier': 4.},
+                      'promotes': ['*']}]}}
+
+        yaml.dump(c, open('simple.yml', 'w'))
+
+        p = FUSEDProblem(filename='simple.yml')
+        p.setup()
+        p.run()
+        self.assertEqual(p['x'], 3.)
+        self.assertEqual(p['y'], 12.)
+
 
 if __name__ == '__main__':
 
